d9687e0cb2d3143ed1708ed1679fd99cfb9ed132
Sander Garland Slinning <sandergs@stud.ntnu.no>
2023-09-29 15:20:03 +0200

    Made and styled a simple navbar(#4)

---
+++ Sounds-Good/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,52 @@
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+export const Navbar = () => {
+  const navigate = useNavigate();
+
+  return (
+    <AppBar sx={{ backgroundColor: "orange" }}>
+      <Container maxWidth="xl">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            sx={{
+              mr: 2,
+              display: "flex",
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            Sounds Good
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: "flex" }}>
+            <Button
+              onClick={() => navigate(`/project2/`)}
+              sx={{ my: 2, color: "white", display: "block" }}
+            >
+              Home
+            </Button>
+            <Button
+              onClick={() => navigate(`/project2/playlists`)}
+              sx={{ my: 2, color: "white", display: "block" }}
+            >
+              Playlists
+            </Button>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
