3f49d5be77238593b66e6442291d07080356e6a2
Snorre Saether <snosaet@hotmail.com>
2023-09-29 14:07:29 +0200

    setup vitest (mui, jest, testing lib, msw) #2

---
+++ Sounds-Good/src/test/setup.ts
@@ -0,0 +1,2 @@
+import "@testing-library/jest-dom";
+//Could import matchers from jest and extending vitest functionality
--- Sounds-Good/vite.config.ts
+++ Sounds-Good/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vitest" />
+//vite/client reference is poorly documented by Vitest, but seems to be needed for our purposes
+/// <reference types="vite/client" />
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  esbuild: {
+    //this for mui testing. Found in mui vitest example on GitHub
+    jsxInject: "import React from 'react'",
+  },
+  test: {
+    //allows for use of Vitest methodes without import:
+    globals: true,
+    //defines testing environemnt for testing web-apps as:
+    environment: "jsdom",
+    //setup file for testing, which imports jest features to be used with vitest
+    setupFiles: "./src/test/setup.ts",
+    //allows for parsing css, so can test css properties if needed:
+    css: true,
+  },
+});
