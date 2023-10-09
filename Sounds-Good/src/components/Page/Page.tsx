a521c3233d43066d73b925981f9a43a8ef2ebf52
Snorre Saether <snosaet@hotmail.com>
2023-10-10 01:36:05 +0200

    creates standard page component #13

---
+++ Sounds-Good/src/components/Page/Page.tsx
@@ -0,0 +1,10 @@
+import "./Page.css";
+import { Children } from "../../types.ts";
+
+type PageProps = {
+  children?: Children;
+};
+export function Page({ children }: PageProps) {
+  return <div className="page">{children}</div>;
+}
+export default Page;
