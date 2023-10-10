87aac49be94de1a74cc3918ab89b83b1563b8572
Snorre Saether <snosaet@hotmail.com>
2023-10-10 12:58:41 +0200

    creates Searchresult page draft #14

--- Sounds-Good/src/App.tsx
+++ Sounds-Good/src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage/Homepage";
+import SearchResultPage from "./pages/SearchResultPage/SearchResultPage.tsx";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/project2" element={<Homepage />} />
+        <Route path="/project2/searchResult" element={<SearchResultPage />} />
       </Routes>
     </>
   );
