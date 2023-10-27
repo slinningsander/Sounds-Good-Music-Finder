c7b68123cfc200a014bd09593a4e325f568113e4
Snorre Saether <snosaet@hotmail.com>
2023-10-27 13:28:12 +0200

    all dataset setup

---
+++ Sounds-Good-Backend/dataset-utils/getTopArtists.py
@@ -0,0 +1,25 @@
+import json
+import requests
+
+
+limit = "100"
+api_key = "691ee9c1ac42404e59e5e2fb690e9451"
+
+top_artists = {}
+
+#for artist in artists:
+api_url = f"http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&limit={limit}&api_key={api_key}&format=json"
+response = requests.get(api_url)
+response.raise_for_status()
+data = response.json()
+
+for artist in data["artists"]["artist"]:
+    artist_name = artist["name"]
+    listeners = int(artist["listeners"])
+    top_artists[artist_name] ={
+        "listeners": listeners
+    }
+
+with open('top100_artists.json', 'w', encoding='utf-8') as file:
+    json.dump(top_artists, file, ensure_ascii=False, indent=4)
+
