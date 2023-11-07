33a4a4b65c3713b4aea66d81477e07ce7944315d
Benjamin Fogalele <benjamin.fogalele@ntnu.no>
2023-11-07 18:07:38 +0100

    Added ArtistCardContainer to the homepage where the search bar will be (#28).

--- Sounds-Good/src/pages/Homepage/Homepage.tsx
+++ Sounds-Good/src/pages/Homepage/Homepage.tsx
@@ -1,9 +1,11 @@
 import Search from '../../components/Search/Search.tsx'
+import ArtistCardContainer from '../../components/ArtistCardContainer/ArtistCardContainer.tsx'
 export default function Homepage() {
   return (
     <>
       <h1>This is the Homepage!</h1>
       <Search />
+      <ArtistCardContainer input="A" />
     </>
   )
 }
