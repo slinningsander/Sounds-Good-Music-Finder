0835c2a49561cfa1e816ff400c92a8a2f8554029
Benjamin Fogalele <benjamin.fogalele@ntnu.no>
2023-11-07 18:04:49 +0100

    Added to the existing query by adding the fetchMore function for pagination. Also changed the way offset and limit is used because it was not working before (#28).

--- Sounds-Good/src/queries/getArtist.tsx
+++ Sounds-Good/src/queries/getArtist.tsx
@@ -1,21 +1,51 @@
 import { gql, useQuery } from '@apollo/client'
 
 const GET_ARTIST = gql`
-  query GetArtist($where: ArtistWhere) {
-    artists(where: $where) {
+  query GetArtist($options: ArtistOptions, $where: ArtistWhere) {
+    artists(options: $options, where: $where) {
       artist_name
     }
   }
 `
 
-export default function GetArtist(input: string, offset: number) {
+export default function GetArtist(
+  input: string,
+  offset: number,
+  more: boolean,
+  setMore: (more: boolean) => void
+) {
   const result = useQuery(GET_ARTIST, {
     variables: {
       where: { artist_name_STARTS_WITH: input },
-      offset: { offset },
-      limit: { limit: 10 },
+      options: {
+        limit: 5,
+        offset: offset,
+      },
     },
   })
+
+  console.log(more)
+  const fetchMoreArtists = () => {
+    result
+      .fetchMore({
+        variables: {
+          options: {
+            limit: 5,
+            offset: offset,
+          },
+        },
+      })
+      .then((res) => {
+        console.log(res)
+      })
+    console.log('fetching more')
+    console.log(result.data)
+    setMore(false)
+  }
+
+  if (more) {
+    fetchMoreArtists()
+  }
 
   return result
 }
