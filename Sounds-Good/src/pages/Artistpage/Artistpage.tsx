5984d98bec00a84e17507363cac8bae8572815d8
Sander Garland Slinning <sandergs@stud.ntnu.no>
2023-11-07 15:44:27 +0100

    Added data from database (#31)

--- Sounds-Good/src/pages/Artistpage/Artistpage.tsx
+++ Sounds-Good/src/pages/Artistpage/Artistpage.tsx
@@ -1,78 +1,59 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useEffect } from 'react'
 import styles from './Artistpage.module.css'
 import Page from '../../components/Page/Page'
+import GetArtist from '../../queries/getArtist'
 
-interface Album {
-  img: string
-  title: string
-}
-
-interface Artist {
-  name: string
-  bio: string
-  albums: Album[]
+type Album = {
+  album_art: string | undefined
+  album_title: string
 }
 
 const Artistpage = () => {
-  const { artistId } = useParams<{ artistId: string }>()
+  const url = new URL(window.location.href)
 
+  // Extract the value of the parameter "name" from the URL
+  const artistName = url.pathname.split('/').pop() // Extracts "Kanye%20West"
+
+  // Decode the URI component to get the actual artist name
+  const decodedArtistName = decodeURIComponent(artistName || '')
+
+  const { data, loading, error } = GetArtist(decodedArtistName)
+
+  const sanitizedBio = data?.artists[0].artist_bio.replace(
+    /<a\b[^>]*>.*?<\/a>/g,
+    ''
+  )
+
+  useEffect(() => {
+    if (loading) {
+      console.log('Loading')
+    }
+    if (error) {
+      console.log(error)
+    }
+    if (data) {
+      console.log(data.artists[0])
+    }
+  }, [data, error, loading])
   // Replace with actual artist data
-  const artist: Artist = {
-    name: 'The Beatles',
-    bio: 'The Beatles were an English rock band formed in Liverpool in 1960. With a line-up comprising John Lennon, Paul McCartney, George Harrison and Ringo Starr, they are regarded as the most influential band of all time.',
-    albums: [
-      {
-        title: 'Please Please Me',
-        img: '//upload.wikimedia.org/wikipedia/commons/thumb/3/33/Are_You_Experienced_-_US_cover-edit.jpg/1920px-Are_You_Experienced_-_US_cover-edit.jpg',
-      },
-      {
-        title: 'With the Beatles',
-        img: '//upload.wikimedia.org/wikipedia/commons/thumb/3/33/Are_You_Experienced_-_US_cover-edit.jpg/1920px-Are_You_Experienced_-_US_cover-edit.jpg',
-      },
-      {
-        title: "A Hard Day's Night",
-        img: '//upload.wikimedia.org/wikipedia/commons/thumb/3/33/Are_You_Experienced_-_US_cover-edit.jpg/1920px-Are_You_Experienced_-_US_cover-edit.jpg',
-      },
-      {
-        title: 'Beatles for Sale',
-        img: '//upload.wikimedia.org/wikipedia/commons/thumb/3/33/Are_You_Experienced_-_US_cover-edit.jpg/1920px-Are_You_Experienced_-_US_cover-edit.jpg',
-      },
-      {
-        title: 'Help!',
-        img: '//upload.wikimedia.org/wikipedia/commons/thumb/3/33/Are_You_Experienced_-_US_cover-edit.jpg/1920px-Are_You_Experienced_-_US_cover-edit.jpg',
-      },
-      {
-        title: 'Rubber Soul',
-        img: '//upload.wikimedia.org/wikipedia/commons/thumb/3/33/Are_You_Experienced_-_US_cover-edit.jpg/1920px-Are_You_Experienced_-_US_cover-edit.jpg',
-      },
-      {
-        title: 'Revolver',
-        img: '//upload.wikimedia.org/wikipedia/commons/thumb/3/33/Are_You_Experienced_-_US_cover-edit.jpg/1920px-Are_You_Experienced_-_US_cover-edit.jpg',
-      },
-      { title: "Sgt. Pepper's Lonely Hearts Club Band", img: 1967 },
-      { title: 'Magical Mystery Tour', img: 1967 },
-      { title: 'The Beatles (White Album)', img: 1968 },
-      { title: 'Yellow Submarine', img: 1969 },
-      { title: 'Abbey Road', img: 1969 },
-      { title: 'Let It Be', img: 1970 },
-    ],
-  }
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error</p>
 
   return (
     <>
       <Page>
         <div className={styles.container}>
           <div className={styles.infoContainer}>
-            <h1>{artist.name}</h1>
-            <p className={styles.bio}>{artist.bio}</p>
+            <h1>{data.artists[0].artist_name}</h1>
+            <p className={styles.bio}>{sanitizedBio}</p>
           </div>
           <h2>Albums</h2>
           <div className={styles.albumContainer}>
-            {artist.albums.map((album, index) => (
-              <div key={index} className={styles.albums}>
-                <p> {album.title}</p>
-                <img src={album.img} className={styles.albumCover} />
+            {data.artists[0].createdAlbumAlbums.map((album: Album) => (
+              <div className={styles.albums}>
+                <p> {album.album_title}</p>
+                <img src={album.album_art} className={styles.albumCover} />
               </div>
             ))}
           </div>
