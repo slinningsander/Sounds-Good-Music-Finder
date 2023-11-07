8ecd4571a0f2933725c844802a44a0cca06248ee
Benjamin Fogalele <benjamin.fogalele@ntnu.no>
2023-11-07 18:10:33 +0100

    Added typepolicy to the Apollo MemoryCache. This is to ensure that the fetched results are cached as one object. Also had to remove React.StrictMode because it led to the fetch happening twice. Lastly, changed the ApolloClient URI because the backend is properly setup on the server (#28).

--- Sounds-Good/src/main.tsx
+++ Sounds-Good/src/main.tsx
@@ -5,16 +5,28 @@ import { BrowserRouter } from 'react-router-dom'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000', //Change this url to the url of the server
-  cache: new InMemoryCache(),
+  uri: 'http://it2810-23.idi.ntnu.no:4000', //Change this url to the url of the server
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          artists: {
+            keyArgs: false,
+            merge(existing = [], incoming) {
+              return [...existing, ...incoming]
+            },
+          },
+        },
+      },
+    },
+  }),
 })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ApolloProvider client={client}>
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ApolloProvider>
 )
+// http://it2810-23.idi.ntnu.no:4000
