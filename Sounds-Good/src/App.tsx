f55d631e281dd149fe4c2648737242281ce487ac
Sander Garland Slinning <sandergs@stud.ntnu.no>
2023-11-07 15:40:30 +0100

    Added correct route (#31)

--- Sounds-Good/src/App.tsx
+++ Sounds-Good/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Homepage from './pages/Homepage/Homepage.tsx'
 import Detailspage from './pages/Detailspage/Detailspage'
 import { Navbar } from './components/Navbar/Navbar'
+import Artistpage from './pages/Artistpage/Artistpage.tsx'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/project2" element={<Homepage />} />
         <Route path="/project2/:artist/:song" element={<Detailspage />} />
+        <Route path="/project2/:artist" element={<Artistpage />} />
       </Routes>
     </>
   )
