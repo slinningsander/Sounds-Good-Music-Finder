2b27cc3ab484074c53f2b24818d491fabaa7737d
bcfogale <bcfogale@ntnu.no>
2023-11-08 01:41:25 +0100

    Created some general code for the relevant query based on the previous query for ArtistCardContainer (#28).

--- Sounds-Good/src/queries/getAlbumsBySearch.tsx
+++ Sounds-Good/src/queries/getAlbumsBySearch.tsx
@@ -1,8 +1,8 @@
 import { gql, useQuery } from '@apollo/client'
 
 const GET_ALBUM = gql`
-  query GetAlbum($where: AlbumWhere) {
-    albums(where: $where) {
+  query GetAlbum($options: AlbumOptions, $where: AlbumWhere) {
+    albums(options: $options, where: $where) {
       album_art
       album_title
       artistsCreatedAlbum {
@@ -12,10 +12,45 @@ const GET_ALBUM = gql`
   }
 `
 
-export default function GetArtist(input: string) {
+export default function GetArtist(
+  input: string,
+  offset: number,
+  more: boolean,
+  setMore: (more: boolean) => void
+) {
   const result = useQuery(GET_ALBUM, {
-    variables: { where: { track_title_STARTS_WITH: input } },
+    variables: {
+      where: { album_title_STARTS_WITH: input },
+      options: {
+        limit: 5,
+        offset: offset,
+      },
+    },
   })
+
+  console.log(more)
+
+  const fetchMoreAlbums = () => {
+    result
+      .fetchMore({
+        variables: {
+          options: {
+            limit: 5,
+            offset: offset,
+          },
+        },
+      })
+      .then((res) => {
+        console.log(res)
+      })
+    console.log('fetching more')
+    console.log(result.data)
+    setMore(false)
+  }
+
+  if (more) {
+    fetchMoreAlbums()
+  }
 
   return result
 }
