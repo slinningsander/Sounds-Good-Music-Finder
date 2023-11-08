575de3163695c9c3f7b7f405c4e2f78252671595
Snorre Saether <snosaet@hotmail.com>
2023-11-08 17:57:39 +0100

    filter by tag with functionality #36

--- Sounds-Good/src/pages/Homepage/Homepage.tsx
+++ Sounds-Good/src/pages/Homepage/Homepage.tsx
@@ -1,9 +1,13 @@
+import { AlbumTagFilter } from '../../components/FilterComponent/subcomponents/AlbumTagFilter/AlbumTagFilter.tsx'
+import { SearchFilter } from '../../components/FilterComponent/subcomponents/SearchFilter/SearchFilter.tsx'
 import Search from '../../components/Search/Search.tsx'
 export default function Homepage() {
   return (
     <>
       <h1>This is the Homepage!</h1>
       <Search />
+      <SearchFilter />
+      <AlbumTagFilter />
     </>
   )
 }
