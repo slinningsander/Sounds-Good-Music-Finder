91df91a99699dfdc20cffe618e38343012651c88
bcfogale <bcfogale@ntnu.no>
2023-11-08 01:25:24 +0100

    Created a general implementation for the albumcontainer. Mostly copied from ArtistCardContainer (#28).

---
+++ Sounds-Good/src/components/AlbumCardContainer/AlbumCardContainer.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react'
+import GetAlbumBySearch from '../../queries/getAlbumsBySearch'
+import AlbumCard from '../AlbumCard/AlbumCard'
+import styles from './AlbumCardContainer.module.css'
+
+type AlbumCardContainerProps = {
+  input: string
+}
+
+const AlbumCardContainer = ({ input }: AlbumCardContainerProps) => {
+  const [offset, setOffset] = useState(0)
+  const [more, setMore] = useState(false)
+  const { data, error, loading } = GetAlbumBySearch(
+    input,
+    offset,
+    more,
+    setMore
+  )
+
+  useEffect(() => {
+    if (loading) {
+      console.log('loading')
+    } else if (error) {
+      console.log(error)
+    } else {
+      console.log(data.albums)
+      setOffset(data.albums.length)
+    }
+  }, [data, error])
+
+  return (
+    <div className={styles.wrapper}>
+      {loading ? (
+        <></>
+      ) : (
+        data.albums.map(
+          (album: {
+            album_title: string
+            album_art: string
+            artistsCreatedAlbum: string
+          }) => (
+            <div>
+              <AlbumCard
+                album={album.album_title}
+                artist={album.artistsCreatedAlbum}
+                img={album.album_art}
+              />
+            </div>
+          )
+        )
+      )}
+
+      <button onClick={() => setMore(true)}>Show More</button>
+    </div>
+  )
+}
+
+export default AlbumCardContainer
