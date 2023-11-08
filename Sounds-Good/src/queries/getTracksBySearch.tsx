d24c3b09e0ba87e420c9c414e334ad6dd45570c1
Benjamin Fogalele <benjamin.fogalele@ntnu.no>
2023-11-08 16:43:35 +0100

    Created the neccessary query for getting tracks (#28).

--- Sounds-Good/src/queries/getTracksBySearch.tsx
+++ Sounds-Good/src/queries/getTracksBySearch.tsx
@@ -1,12 +1,12 @@
 import { gql, useQuery } from '@apollo/client'
 
 const GET_TRACK = gql`
-  query GetTrack($where: TrackWhere) {
-    tracks(where: $where) {
-      cover_art
+  query GetTrack($options: TrackOptions, $where: TrackWhere) {
+    tracks(options: $options, where: $where) {
       track_title
       albumsHasTrack {
         album_title
+        album_art
       }
       artistsCreatedTrack {
         artist_name
@@ -15,14 +15,45 @@ const GET_TRACK = gql`
   }
 `
 
-export default function GetArtist(input: string, offset: number) {
+export default function GetArtist(
+  input: string,
+  offset: number,
+  more: boolean,
+  setMore: (more: boolean) => void
+) {
   const result = useQuery(GET_TRACK, {
     variables: {
       where: { track_title_STARTS_WITH: input },
-      offset: { offset },
-      limit: { limit: 10 },
+      options: {
+        limit: 5,
+        offset: offset,
+      },
     },
   })
+
+  console.log(more)
+
+  const fetchMoreTracks = () => {
+    result
+      .fetchMore({
+        variables: {
+          options: {
+            limit: 5,
+            offset: offset,
+          },
+        },
+      })
+      .then((res) => {
+        console.log(res)
+      })
+    console.log('fetching more')
+    console.log(result.data)
+    setMore(false)
+  }
+
+  if (more) {
+    fetchMoreTracks()
+  }
 
   return result
 }
