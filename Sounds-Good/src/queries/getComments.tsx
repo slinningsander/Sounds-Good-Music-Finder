48ae2c117637e18d2344aaecfe2e13903a9b3752
Sander Garland Slinning <sandergs@stud.ntnu.no>
2023-11-10 16:38:18 +0100

    Add getComments query for Apollo client (#44)

---
+++ Sounds-Good/src/queries/getComments.tsx
@@ -0,0 +1,32 @@
+import { gql, useQuery } from '@apollo/client'
+
+const GET_COMMENTS = gql`
+  query Comments($where: CommentWhere) {
+    comments(where: $where) {
+      text
+    }
+  }
+`
+export default function GetComments(
+  track_title: string | undefined,
+  album_title: string | undefined,
+  artist_name: string | undefined
+) {
+  const result = useQuery(GET_COMMENTS, {
+    variables: {
+      where: {
+        commentedOnTrack_SINGLE: {
+          track_title: track_title,
+          artistsCreatedTrack_SINGLE: {
+            artist_name: artist_name,
+          },
+          albumsHasTrack_SINGLE: {
+            album_title: album_title,
+          },
+        },
+      },
+    },
+  })
+
+  return result
+}
