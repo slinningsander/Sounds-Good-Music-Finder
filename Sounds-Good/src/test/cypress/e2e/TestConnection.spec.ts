a025938625921c14698f18cfb496785f5387d2e0
Benjamin Fogalele <benjamin.fogalele@ntnu.no>
2023-11-14 14:02:04 +0100

    Changed the baseURL to Cypress to make it easier to navigate in later tests (#17).

--- Sounds-Good/src/test/cypress/e2e/TestConnection.spec.ts
+++ Sounds-Good/src/test/cypress/e2e/TestConnection.spec.ts
@@ -1,5 +1,5 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Application', () => {
+  it('Test connection to application', () => {
+    cy.visit('')
   })
 })
