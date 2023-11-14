60883ab780e180124a1893b5f7dad87587ab5bb4
Sander Garland Slinning <sandergs@stud.ntnu.no>
2023-11-14 19:42:23 +0100

    Fix minDuration state initialization in (#46)

--- Sounds-Good/src/pages/Homepage/Homepage.tsx
+++ Sounds-Good/src/pages/Homepage/Homepage.tsx
@@ -12,7 +12,7 @@ export default function Homepage() {
   const [searchbarValue, setSearchbarValue] = useState('')
   const [selectedValue, setSelectedValue] = useState('TRACK')
   const [maxDuration, setMaxDuration] = useState(600)
-  const [minDuration, setMinDuration] = useState()
+  const [minDuration, setMinDuration] = useState(0)
   const [sortingDirection, setSortingDirection] = useState('ASC') // State for sorting direction
 
   const handleSortingChange = (event: any) => {
