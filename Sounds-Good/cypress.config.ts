96efb2687f512ea217c0423addf62416b3dab787
Benjamin Fogalele <benjamin.fogalele@ntnu.no>
2023-11-14 13:49:26 +0100

    Changed the path for the cypress folders, so they end up in our test folder (#17).

--- Sounds-Good/cypress.config.ts
+++ Sounds-Good/cypress.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  downloadsFolder: 'src/test/cypress/downloads',
+  videosFolder: 'src/test/cypress/videos',
+  screenshotsFolder: 'src/test/cypress/screenshots',
   e2e: {
+    specPattern: 'src/test/cypress/e2e/*.spec.ts',
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+})
--- Sounds-Good/cypress/e2e/spec.cy.ts
+++
@@ -1,5 +0,0 @@
-describe('template spec', () => {
-  it('Visits the page', () => {
-    cy.visit('http://it2810-23.idi.ntnu.no/project2')
-  })
-})
