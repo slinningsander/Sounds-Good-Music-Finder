441359ae824a3a452c87c8faedce22284a63d40f
Snorre Saether <snosaet@hotmail.com>
2023-11-22 17:37:19 +0100

    duration filter state in redux #66

---
+++ Sounds-Good/src/redux/slices/filterDurationSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { RootState } from '../store'
+
+interface FilterDurationState {
+  value: number[]
+}
+
+const initialState: FilterDurationState = {
+  value: [0, 600],
+}
+
+export const filterDurationSlice = createSlice({
+  name: 'filterDuration',
+  initialState,
+  reducers: {
+    updateDurationFilter: (state, action) => {
+      state.value = action.payload
+    },
+  },
+})
+
+export const { updateDurationFilter } = filterDurationSlice.actions
+export const selectFilterDuration = (state: RootState) =>
+  state.filterDuration.value
+export default filterDurationSlice.reducer
