63f73dc0fa6ec2765aacb803b716b61e091801cc
Sander Garland Slinning <sandergs@stud.ntnu.no>
2023-11-29 21:16:24 +0100

    Renamed folders to "frontend" and "backend" (#91)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Homepage from './pages/Homepage/Homepage.tsx'
 import Songpage from './pages/Songpage/Songpage.tsx'
-import { Navbar } from './components/Navbar/Navbar'
+import { Navbar } from './components/Navbar/Navbar.tsx'
 import Artistpage from './pages/Artistpage/Artistpage.tsx'
 import Albumpage from './pages/Albumpage/Albumpage.tsx'
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import { Provider } from 'react-redux'
-import store from './redux/store'
+import store from './redux/store.ts'
 
 const client = new ApolloClient({
   uri: 'http://it2810-23.idi.ntnu.no:4000', //Change this url to the url of the server
